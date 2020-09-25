7d4703d40821f711bdb6b4ce480634dcb452e53b
Weihua Zhu <zhuweihua2746@gmail.com>
2020-09-24 21:36:06 -0700

    init

---
+++ consts/cities.py
@@ -0,0 +1,5 @@
+EUROPE_CITIES = frozenset([
+    'London', 'Manchester', 'France', 'Paris', 'Belgium', 'Brussels', 'Netherlands', 'Amsterdam', 'Rotterdam',
+    'Germany', 'Berlin', 'Stuttgart', 'Tubingen', 'Austria', 'Vienna', 'Switzerland', 'Zurich', 'Italy', 'Milan',
+    'Venice', 'San Marino', 'Rome', 'Spain', 'Barcelona'
+])
