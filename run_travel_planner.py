14ea377865641aac1670aa100b5a4c25b05f531d
Weihua Zhu <zhuweihua2746@gmail.com>
2020-09-24 21:46:48 -0700

    main function init

--- run_travel_planner.py
+++ run_travel_planner.py
@@ -1,4 +1,5 @@
 from logic.distance_calculate_logic import calculate_pair_distances
 from logic.shortest_path_logic import calculate_shortest_path
 
-print("===================== travel planner starts ===========================")
+if __name__ == '__main__':
+    print("hello world")
